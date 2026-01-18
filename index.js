932e3a1f20c7934a83e6eadde1b6b85391167552
rudecasbau <rudecasbau@gmail.com>
2026-01-18 13:38:42 +0100

    Cambio en el backend

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require('mongoose');
 const fs = require("fs");
 const path = require("path");
+const User = require("./models/User_temp");
 require("dotenv").config();
 
-const mongoose = require('mongoose');
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Conexión exitosa a MongoDB Atlas"))
