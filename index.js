66f0363474a7052a359eace3e8728b5598729a94
rudecasbau <rudecasbau@gmail.com>
2026-01-18 13:49:53 +0100

    Cambio en el backend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require('mongoose');
 const fs = require("fs");
 const path = require("path");
-const User = require("./models/User_temp");
+const User = require("./models/User");
 require("dotenv").config();
 
 
